011dac02c5c86820b207d6e220a2812dbeef7371
REKSOFT\chaplygin <chaplygin@reksoft.ru>
2018-07-24 13:26:44 +0300

    Refactoring

--- src/main/java/ru/reksoft/chaplygin/catalog/services/SectorService.java
+++ src/main/java/ru/reksoft/chaplygin/catalog/services/SectorService.java
@@ -3,7 +3,6 @@ package ru.reksoft.chaplygin.catalog.services;
 import ru.reksoft.chaplygin.catalog.dto.SectorDTO;
 import ru.reksoft.chaplygin.catalog.entity.Sector;
 
-import java.util.List;
 import java.util.Set;
 
 public interface SectorService {
